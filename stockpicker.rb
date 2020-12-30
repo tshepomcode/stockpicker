43d2f88157702a21c302abefc7df662fe050ecdf
tshepomcode <tshepomcode@gmail.com>
2020-12-30 09:42:55 +0200

    method completed

--- stockpicker.rb
+++ stockpicker.rb
@@ -9,42 +9,65 @@
 
 require 'pry'
 
-# def stock_picker
+def stock_picker(prices)
+  high_price = prices.max
+  prices_list = high_price == prices.first ? (prices.select {|price| price != high_price}) : []
+  prices_list.empty? ? () : high_price = prices_list.max
+  high_price_index = prices.index(high_price)
 
-# end
+  if prices_list.empty? 
+    low_price = prices[0..(high_price_index)].min
+ else 
+   low_price = prices_list[0..(high_price_index)].min
+ end
+
+ low_price_index = prices.index(low_price)
+ profit = high_price - low_price
+ p "[#{low_price_index},#{high_price_index}] # for profit of $#{high_price} - $#{low_price} == $#{profit}"
+
+end
 
 # ##### Workbench #####
 # to use Pry-byebug use binding.pry
 # binding.pry
 
 prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+# # prices = [8, 11, 4, 7, 16, 6, 3, 18, 1]
 
-high_price = prices.max
-p "high_price: #{high_price}"
+stock_picker(prices)
 
-# new array with new high price if high price is first in the arrary
-prices_no_first_high = high_price == prices.first ? (prices.select {|price| price != high_price}) : []
+##### Workbench #####
+# high_price = prices.max
+# p "high_price: #{high_price}"
 
-# check in new array is not empty, if empty, the high price was not first
-p "new array has #{prices_no_first_high}"
-prices_no_first_high.empty? ? () : high_price == prices_no_first_high.max
+# # new array with new high price if high price is first in the arrary
+# prices_list = high_price == prices.first ? (prices.select {|price| price != high_price}) : []
+
+# # check in new array is not empty, if empty, the high price was not first
+# p "new array has #{prices_list}"
+# prices_list.empty? ? () : high_price = prices_list.max
+
+# # high_price == prices.first ? (high_price = (prices.select {|price| price != high_price}).max) : ()
+
+# #find location of highest in main array
+# high_price_index = prices.index(high_price)
+
+# #find lowest price starting from the highest price in array
+# # new array from highest price that is not first
+# if prices_list.empty? 
+#    low_price = prices[0..(high_price_index)].min
+# else 
+#   low_price = prices_list[0..(high_price_index)].min
+# end
+# # low_price = prices_list[0..(high_price_index)].min
+# low_price_index = prices.index(low_price)
+# profit = high_price - low_price
+
+# p "high_price_index = #{high_price_index}"
+# p "low_price_index = #{low_price_index}"
 
 
+# p "low_price = #{low_price}"
+# p "high_price: #{high_price}"
 
-high_price == prices.first ? (high_price = (prices.select {|price| price != high_price}).max) : ()
-
-#find location of highest
-high_price_index = prices.index(high_price)
-
-#find lowest price starting from the highest price in array
-# new array from highest price that is not first
-low_price = prices_no_first_high[0..4].min
-
-p "high_price_index = #{high_price_index}"
-p "low_price = #{low_price}"
-
-# low_two = prices.min(2)
-# top_two = prices.max(2)
-
-p "high_price: #{high_price}"
-
+# p "[#{low_price_index},#{high_price_index}] # for profit of $#{high_price} - $#{low_price} == $#{profit}"
